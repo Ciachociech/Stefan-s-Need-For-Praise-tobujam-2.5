5a35d72ff49f70af05de4362277ebaaf558dd6d1
Mateusz Chojnacki <matchojnacki@protonmail.com>
2024-11-23 14:09:48 +0100

    Add bar frames and icon placeholders

--- game/scenes/Statistics.py
+++ game/scenes/Statistics.py
@@ -27,6 +27,25 @@ class Statistics(common.Scene):
         return False
 
     def render(self, color=pygame.Color(255, 255, 255, 255)):
-        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(0, 0, 0, 255), (60, 5 * self.animation_frames / 2 - 240, 600, 240))
-        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255), (60, 5 * self.animation_frames / 2 - 240, 600, 240),
-                                         width=8)
+        frame_shift = 4 * self.animation_frames / 2 - 200
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(0, 0, 0, 255), (60, frame_shift, 600, 200))
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (60, frame_shift, 600, 200), width=8)
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (180, frame_shift + 35, 160, 40), width=4, border_radius=12)
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (180, frame_shift + 119, 160, 40), width=4, border_radius=12)
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (472, frame_shift + 35, 160, 40), width=4, border_radius=12)
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (472, frame_shift + 119, 160, 40), width=4, border_radius=12)
+
+        # TODO: replace with icons
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (92, frame_shift + 23, 64, 64))
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (92, frame_shift + 107, 64, 64))
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (384, frame_shift + 23, 64, 64))
+        frame_surface = pygame.draw.rect(self.window.window, pygame.Color(255, 255, 255, 255),
+                                         (384, frame_shift + 107, 64, 64))
